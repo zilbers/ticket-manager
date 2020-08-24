6c2343dcb81c5fa12c30cb43e065ebe62f5a309c
Omri Zilberstein <67857576+zilbers@users.noreply.github.com>
2020-08-24 10:58:28 +0300

    Set up project

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      Hello World
+    </main>
+  );
+}
+
+export default App;
