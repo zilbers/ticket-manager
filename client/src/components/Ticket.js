cc32066bb63d9322c505109734bcc5022d3def94
Omri Zilberstein <67857576+zilbers@users.noreply.github.com>
2020-08-25 00:54:59 +0300

    Update Ticket.js
    Client passes 5 tests

--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
 import Label from './Label';
 import Info from './Info';
 import './Ticket.css';
@@ -12,14 +14,24 @@ function Ticket(props) {
       className="ticket"
       onMouseEnter={() => setHideButton(false)}
       onMouseLeave={() => setHideButton(true)}
+      onMouseOver={() => setHideButton(false)}
+      onFocus={() => setHideButton(false)}
     >
       <span
         className="hideTicketButton"
         hidden={hideButton}
-        onClick={() => props.hideTicket(ticket)}
       >
-        Hide
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          className="hideTicketButton"
+          onClick={() => props.hideTicket(ticket)}
+        >
+          <CloseIcon />
+        </IconButton>
       </span>
+
       <div className="body">
         <h4 className="title">{ticket.title}</h4>
         <p className="content">{ticket.content}</p>
