ee5ddabc472cb6062e8951207fb672478207c8c0
Omri Zilberstein <67857576+zilbers@users.noreply.github.com>
2020-08-24 12:06:59 +0300

    Update app.js
    Started building entry points

--- server/app.js
+++ server/app.js
@@ -1,5 +1,32 @@
 const express = require('express');
+const fs = require('fs').promises;
 
 const app = express();
+let requestID = 0;
 
 module.exports = app;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use('/', express.static('../client/build/'));
+
+function logger(req, res, next) {
+  requestID += 1;
+  console.log(`Request fired: ${req.url} 
+Method: ${req.method}
+Request #${requestID}
+`);
+  next();
+}
+
+app.use(logger);
+
+app.get('api/tickets/', async (req, res) => {
+  try {
+    const content = await fs.readFile('./data.json');
+    const json = JSON.parse(content);
+    res.send(json);
+  } catch (error) {
+    res.send(error);
+  }
+});
