cdc422177359149ce01bc53736d03931ddf94701
Omri Zilberstein <67857576+zilbers@users.noreply.github.com>
2020-08-24 12:06:01 +0300

    Prepared files

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -10,5 +10,13 @@ module.exports = {
     ecmaVersion: 12,
   },
   rules: {
+    'no-console': 'off',
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+        message: 'Unexpected property on console object was called',
+      },
+    ],
   },
 };
