caf3f4532821c3da14adce918a1e6280fc0f4fee
Omri Zilberstein <67857576+zilbers@users.noreply.github.com>
2020-08-27 11:35:00 +0300

    Fixing github tests

--- client/src/app.test.js
+++ client/src/app.test.js
@@ -87,10 +87,7 @@ jest.setTimeout(30000);
 const projectName = '1.Ticket Manager UI';
 describe(`${projectName} - second test suite`, () => {
   beforeAll(async () => {
-    browser = await puppeteer.launch({
-      headless: false,
-      slowMo: 50,
-    });
+    browser = await puppeteer.launch({ });
     page = await browser.newPage();
     useNock(page, ['http://localhost:3000/api']);
 
